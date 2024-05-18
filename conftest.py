b6d3ea5b10a968fb38ff7bf7d514a02046ff4b69
Berkutova Leilia <Gadievalelli@yandex.ru>
2024-05-18 18:26:44 +0300

    обновление

--- conftest.py
+++ conftest.py
@@ -1,4 +1,10 @@
-@pytest.fixture()
+import pytest
+from selene import browser
+
+
+@pytest.fixture
 def setting_browser():
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
     yield
     browser.quit()
